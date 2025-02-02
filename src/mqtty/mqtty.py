806869a949d2b78c8efe0cf1f3936e074b3fe368
Mark Featherston <mark@embeddedts.com>
2025-02-01 21:33:46 -0700

    Add support for web sockets

--- src/mqtty/mqtty.py
+++ src/mqtty/mqtty.py
@@ -12,16 +12,20 @@ import sys
 class MQTTY:
     def __init__(self, mqtt_uri):
         self.mqtt_uri = urlparse(mqtt_uri)
-        if self.mqtt_uri.scheme != "mqtt":
-            raise ValueError("Invalid URI scheme. Expected 'mqtt://'.")
+
+        if self.mqtt_uri.scheme not in ["mqtt", "ws", "wss"]:
+            raise ValueError("Invalid URI scheme. Expected 'mqtt://', 'ws://', or 'wss://'.")
 
         self.mqtt_host = self.mqtt_uri.hostname
-        self.mqtt_port = self.mqtt_uri.port or 1883  # Default MQTT port
+        self.mqtt_port = self.mqtt_uri.port or (443 if self.mqtt_uri.scheme == "wss" else 80)
+        self.mqtt_transport = "websockets" if self.mqtt_uri.scheme in ["ws", "wss"] else "tcp"
+
         base_path = self.mqtt_uri.path.lstrip("/")  # Remove leading slash from path
         self.device_serial_input_topic = f"{base_path}/device_serial_input"
         self.device_serial_output_topic = f"{base_path}/device_serial_output"
 
-        self.mqtt_client = mqtt.Client(callback_api_version=CallbackAPIVersion.VERSION2)
+        self.mqtt_client = mqtt.Client(transport=self.mqtt_transport, callback_api_version=CallbackAPIVersion.VERSION2)
+
         self.master_fd, self.slave_fd = pty.openpty()  # Create the pseudoterminal
         self.slave_name = os.ttyname(self.slave_fd)
 
@@ -105,6 +109,7 @@ def main():
             bridge.run()
         except ValueError as e:
             sys.stderr.write(f"Error: {e}\n")
+
 
 if __name__ == "__main__":
     main()
